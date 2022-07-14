84f9d2f3a87abe6db9008d94c2e52ba14b3a4e7c
Rafael Costa <93993679+rafaelbhcosta@users.noreply.github.com>
2022-07-14 11:14:22 -0300

    Update main.py

---
+++ projeto/duck_ota_v1.0/main.py
@@ -0,0 +1,9 @@
+print('Duck_OTA')
+
+# Se você conseguiu fazer tudo certo, o print acima deve aparecer no relatório do seu equipamento
+
+# Procure mudar o que está no script e veja atualizar
+
+# Detalhe: em alguns equipamento entre uma atualização e outra pode levar alguns minutos
+# Na minha ESP32 atualiza uma vez a cada uns 3 minutos sem nenhum erro
+# Já na minha LoRa 32 atualiza direto sem intervalos de tempo 
